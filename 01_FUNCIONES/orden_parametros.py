f092b4e41cf2db9da5ace24f8adc0114a7a2952a
ingenierooscar <dante512@hotmail.com>
2022-10-25 19:51:28 -0500

    funcion y parametros

---
+++ 01_FUNCIONES/orden_parametros.py
@@ -0,0 +1,10 @@
+"""las funciones pueden recibir diferentes tiepos de parametros pero siempre con un orden especifico
+   parametros normales, despues *args y por ultimo **kwargs
+   aja porque si
+"""
+
+def parametros_ordenados(nombre, apellido, *args, **kwargs):
+    pass
+
+def parametros_a_la_loca(nombre, apellido, **kwargs, *args):
+    pass
---
+++ 01_FUNCIONES/parametros_args.py
@@ -0,0 +1,23 @@
+from ast import arg
+
+
+def calcular_perimetro(lado_1, lado_2, lado_3, lado_4):
+    perimetro = lado_1 + lado_2 + lado_3 + lado_4
+    return perimetro
+
+perimetro = calcular_perimetro(1,2,3,4)
+print(perimetro)
+
+#hacer lo mismo pero con un for y con *args
+
+def calcular_parametro_args(*args):
+    perimetro = 0
+    for lado in args:
+        perimetro += lado
+    return perimetro
+
+perimetro = calcular_parametro_args(1,2,3,4)
+print(perimetro)
+
+triangulo = calcular_parametro_args(1,2,3)
+print(triangulo)
