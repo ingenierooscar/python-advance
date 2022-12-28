ad215e548f49e79fca6a411d54526c03f86001f4
orodriguez <orodriguez@resurety.com>
2022-12-27 20:43:08 -0500

    archivos csv

---
+++ 05_ARCHIVOS_CSV/crear_scv.py
@@ -0,0 +1,15 @@
+import csv
+import os 
+
+#definir rutas
+ruta_relativa = "csv_vacio.scv"
+ruta_absoluta = "/Users/oscarrodriguez/Documents/PYTHON COURSE ADVENCE/05_ARCHIVOS_CSV/csv_vacio.csv"
+ruta_absoluta_os = os.path.join(os.getcwd(), "csv_vacio.csv")
+
+print(ruta_absoluta)
+print(ruta_absoluta_os)
+
+"""archivo_abierto = open(ruta_relativa, "w")
+writer = csv.writer(archivo_abierto, delimiter=",")
+archivo_abierto.close()
+"""
